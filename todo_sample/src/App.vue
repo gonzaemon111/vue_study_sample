b7ce126776a32c7117c47d6bd07968934484f5e7
GokiSakuraba <goki14914@icloud.com>
2018-10-13 22:03:49 +0900

    complete for feature/assign_label

--- todo_sample/src/App.vue
+++ todo_sample/src/App.vue
@@ -5,10 +5,26 @@
       <li v-for="task in tasks" v-bind:key="task.id">
         <input type="checkbox" v-bind:checked="task.done" v-on:change="toggleTaskStatus(task)">
         {{ task.name }}
+        -
+        <span v-for="id in task.labelIds" v-bind:key="id">
+          {{ getLabelName(id) }}
+        </span>
       </li>
+
       <form v-on:submit.prevent="addTask">
         <input type="text" v-model="newTaskName" placeholder="新しいタスクを入力してください。">
       </form>
+      <div>
+        <h2>ラベル一覧</h2>
+        <li v-for="label in labels" v-bind:key="label.id">
+          <input type="checkbox" v-bind:value="label.id" v-model="newTaskLabelIds">
+          {{ label.text }}
+        </li>
+
+        <form v-on:submit.prevent="addLabel">
+          <input type="text" v-model="newLabelText" placeholder="新しいラベルを入力してください。">
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -19,7 +35,9 @@ export default {
     return {
       // 入力中の新しいタスク名を一時的に保持
       // また、この部分はnewTaskNameの初期値でもある。
-      newTaskName: ''
+      newTaskName: '',
+      newLabelText: '',
+      newTaskLabelIds: []
     }
   },
   methods: {
@@ -39,11 +57,27 @@ export default {
         // storeの`toggleTaskStatus`の第二引数は{id}なので、この形で指定する。
         id: task.id
       })
+    },
+    // ラベルの追加
+    addLabel () {
+      this.$store.commit('addLabel', {
+        text: this.newLabelText
+      })
+      // storeすると、初期値に戻す。
+      this.newLabelText = ''
+    },
+    // labelのIDから、そのラベルのnameを返す。
+    getLabelName (id) {
+      const label = this.labels.filter(label => label.id === id)[0]
+      return label.text
     }
   },
   computed: {
     tasks () {
       return this.$store.state.tasks // ストアを読む
+    },
+    labels () {
+      return this.$store.state.labels
     }
   }
 }
--- todo_sample/src/store.js
+++ todo_sample/src/store.js
@@ -14,28 +14,65 @@ const store = new Vuex.Store({
       {
         id: 1,
         name: '牛乳を買う',
+        labelIds: [1, 3],
         done: false
       },
       {
         id: 2,
         name: 'Vue.jsの本を買う。',
+        labelIds: [1, 2],
+        done: false
+      },
+      {
+        id: 3,
+        name: 'Vueを勉強する。',
+        labelIds: [2, 3],
         done: false
       }
     ],
-    nextTaskId: 3
+    labels: [
+      {
+        id: 1,
+        text: '買い物'
+      },
+      {
+        id: 2,
+        text: '宿題'
+      },
+      {
+        id: 3,
+        text: '家事'
+      }
+    ],
+    nextTaskId: 4,
+    nextLabelId: 4
   },
 
   // mutation
   mutations: {
-    addTask (state, { name }) {
+    // タスクの追加
+    addTask (state, { name, labelIds }) {
       state.tasks.push({
         id: state.nextTaskId,
+        labelIds,
         name,
         done: false
       })
-
       // 次に追加するタスクに付与するIDを更新する
+      state.nextTaskId++
     },
+
+    // ラベルの追加
+    addLabel (state, { text }) {
+      state.labels.push({
+        id: state.nextLabelId,
+        text
+      })
+      // 次に追加するラベルに付与するIDを更新する
+      state.nextLabelId++
+    },
+
+    // 完了->未完了, 未完了->完了にするメソッド
     toggleTaskStatus (state, { id }) {
       // filterメソッド: コールバック関数で指定された条件を満たす配列の要素を返します。
       // つまり、stateのtasksのidを引っ張ってきて、それが、taskのidと一致しているかどうかを考える。
